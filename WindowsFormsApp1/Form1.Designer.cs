65e57e97a9649cc00cc06c203133f43e33a6746f
Ozgur Hepsag <ozgurhepsag@gmail.com>
2019-11-02 02:09:47 +0300

    Gini implemented.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxInput = new System.Windows.Forms.GroupBox();
+            this.browseFileName = new System.Windows.Forms.TextBox();
+            this.browseFileBtn = new System.Windows.Forms.Button();
+            this.groupBoxInput.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxInput
+            // 
+            this.groupBoxInput.Controls.Add(this.browseFileName);
+            this.groupBoxInput.Controls.Add(this.browseFileBtn);
+            this.groupBoxInput.Location = new System.Drawing.Point(13, 13);
+            this.groupBoxInput.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBoxInput.Name = "groupBoxInput";
+            this.groupBoxInput.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBoxInput.Size = new System.Drawing.Size(814, 69);
+            this.groupBoxInput.TabIndex = 2;
+            this.groupBoxInput.TabStop = false;
+            this.groupBoxInput.Text = "Import";
+            // 
+            // browseFileName
+            // 
+            this.browseFileName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.browseFileName.Location = new System.Drawing.Point(22, 23);
+            this.browseFileName.Margin = new System.Windows.Forms.Padding(4);
+            this.browseFileName.Name = "browseFileName";
+            this.browseFileName.ReadOnly = true;
+            this.browseFileName.Size = new System.Drawing.Size(662, 22);
+            this.browseFileName.TabIndex = 0;
+            this.browseFileName.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // browseFileBtn
+            // 
+            this.browseFileBtn.Location = new System.Drawing.Point(706, 19);
+            this.browseFileBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.browseFileBtn.Name = "browseFileBtn";
+            this.browseFileBtn.Size = new System.Drawing.Size(100, 28);
+            this.browseFileBtn.TabIndex = 0;
+            this.browseFileBtn.Text = "Browse...";
+            this.browseFileBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 111);
+            this.Controls.Add(this.groupBoxInput);
+            this.Name = "Form1";
+            this.Text = "ML Classsification Tool";
+            this.groupBoxInput.ResumeLayout(false);
+            this.groupBoxInput.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBoxInput;
+        private System.Windows.Forms.TextBox browseFileName;
+        private System.Windows.Forms.Button browseFileBtn;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using weka.core.converters;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private static weka.core.Instances insts;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.browseFileBtn.Click += BrowseFileBtn_Click;
+        }
+
+        private void BrowseFileBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse Data Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "arff",
+                Filter = "arff files (*.arff)|*.arff",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                browseFileName.Text = openFileDialog1.FileName;
+                insts = new weka.core.Instances(new java.io.FileReader(browseFileName.Text));
+                PrepareDataset();
+            }
+        }
+
+        public void PrepareDataset()
+        {
+            List<bool> isNumeric = new List<bool>();
+            List<bool> isMultiCategorigal = new List<bool>();
+            List<List<string>> numericColumns = new List<List<string>>();
+
+            for (int i = 0; i < insts.numAttributes(); i++)
+            {
+                bool isNum = insts.attribute(i).isNumeric();
+                isNumeric.Add(isNum);
+
+                if(isNum == true)
+                {
+                    numericColumns.Add(new List<string>());
+
+                    for (int j = 0; j < insts.numInstances(); j++)
+                    {
+                        //Console.WriteLine(insts.instance(j).toString(i));
+                        numericColumns[numericColumns.Count - 1].Add(insts.instance(j).toString(i));
+                    }
+                }
+            }
+
+            weka.filters.unsupervised.attribute.Discretize myDiscretize = new weka.filters.unsupervised.attribute.Discretize();
+            myDiscretize.setInputFormat(insts);
+            myDiscretize.setFindNumBins(true);
+            insts = weka.filters.Filter.useFilter(insts, myDiscretize);
+
+            List<List<string>> atrs = new List<List<string>>();
+
+            for (int i = 0; i < insts.numAttributes(); i++)
+            {
+                atrs.Add(new List<string>());
+                for (int j = 0; j < insts.attribute(i).numValues(); j++)
+                {
+                    string sub_category = insts.attribute(i).value(j);
+                    string temp = sub_category.Replace("'", string.Empty);
+                    atrs[atrs.Count - 1].Add(temp);
+                    //Console.Write(sub_category);
+                }
+
+                if(atrs[atrs.Count - 1].Count == 2)
+                {
+                    isMultiCategorigal.Add(true);
+                }
+                else
+                {
+                    isMultiCategorigal.Add(false);
+                }
+                //Console.WriteLine(atrs[atrs.Count - 1].Count);
+            }
+
+            List<List<string>> lst = new List<List<string>>();
+
+            for (int i = 0; i < insts.numInstances(); i++)
+            {
+                lst.Add(new List<string>());
+
+                for (int j = 0; j < insts.instance(i).numValues(); j++)
+                {
+                    string temp = insts.instance(i).toString(j);
+                    temp = temp.Replace("\\", string.Empty);
+                    temp = temp.Replace("'", string.Empty);
+                    lst[lst.Count - 1].Add(temp);
+                
+                    //Console.Write(temp + " ");
+                }
+                //Console.WriteLine();
+            }
+
+            List<List<string>> giniDataset = ConvertToNumericWithGini(lst, atrs);
+            List<List<string>> twoingDataset = ConvertToNumericWithTwoing();
+        }
+
+        public List<List<string>> ConvertToNumericWithGini(List<List<string>> lst, List<List<string>> atrs)
+        {
+            List<List<string>> numericDataset = new List<List<string>>();
+            List<string> targetCol = new List<string>();
+            int targetAtrNum = atrs[atrs.Count - 1].Count;
+            List<string> targetAtrs = atrs[atrs.Count - 1];
+            int totalInst = lst.Count;
+            int columnNum = lst[0].Count;
+
+            for (int i = 0; i < columnNum - 1; i++) // fill the table
+            {
+                List<string> giniScores = new List<string>();
+                int atrNum = atrs[i].Count;
+                List<string> columnAtrs = atrs[i];
+                int[,] allCounts = new int[targetAtrNum, atrNum * 2];
+
+                for (int j = 0; j < totalInst; j++)
+                {
+                    int k = targetAtrs.IndexOf(lst[j][columnNum - 1]);
+                    int l = columnAtrs.IndexOf(lst[j][i]) * 2;
+                    allCounts[k, l]++;
+                    for (int x = 1; x < atrNum * 2; x = x + 2)
+                    {
+                        if(x != l + 1)
+                            allCounts[k, x]++;
+                    }
+                    targetCol.Add(lst[j][columnNum - 1]);
+                }
+
+                for (int j = 0; j < totalInst; j++) // calculate gini from table
+                {
+                    int l = columnAtrs.IndexOf(lst[j][i]) * 2;
+
+                    int denominatorL = 0;
+                    int denominatorR = 0;
+
+                    for (int x = 0; x < allCounts.GetLength(0); x++)
+                    {
+                        denominatorL = allCounts[x,l] + denominatorL;
+                    }
+                    denominatorR = totalInst - denominatorL;
+
+                    double giniL = 1.0;
+                    for (int x = 0; x < allCounts.GetLength(0); x++)
+                    {
+                        giniL = giniL - Math.Pow(Convert.ToDouble(allCounts[x, l]) / Convert.ToDouble(denominatorL), 2.0);
+                    }
+
+                    double giniR = 1.0;
+                    for (int x = 0; x < allCounts.GetLength(0); x++)
+                    {
+                        giniR = giniR - Math.Pow(Convert.ToDouble(allCounts[x, l + 1]) / Convert.ToDouble(denominatorR), 2.0);
+                    }
+
+                    double gini = (giniR * denominatorR + giniL * denominatorL) / totalInst;
+                    giniScores.Add(gini.ToString());
+                }
+
+                numericDataset.Add(giniScores);
+            }
+
+            numericDataset.Add(targetCol);
+
+            return numericDataset;
+        }
+
+        public List<List<string>> ConvertToNumericWithTwoing()
+        {
+            return new List<List<string>>();
+        }
+
+        public void ChangeBackNumericalColumns()
+        {
+
+        }
+
+        public void Arrange2CategoricalColumns()
+        {
+
+        }
+
+        public void WriteFile()
+        {
+
+        }
+
+    }
+}
